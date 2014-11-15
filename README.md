0b8e80c715aa6face7fd0255ed9c8708405f5f4c
Paul Frazee <pfrazee@gmail.com>
2014-11-15 11:34:40 -0600

    switched to 9-bit encoding; updated toCustom() api; added emojis.json

--- README.md
+++ README.md
@@ -12,7 +12,6 @@ becomes
 
 :bulb::credit_card::baggage_claim::boar::church::dizzy::arrow_heading_down::busstop::construction::department_store::candy::bank::bath::arrow_right::baby_bottle::boar::arrow_left::egg::dvd::cop::children_crossing::battery::100::copyright::clock12::birthday::confounded::blossom::abcd::cake::disappointed::earth_asia:
 
-
 ## api
 
 ```js
@@ -22,8 +21,12 @@ baseEmoji.toUtf8(buf)
 // => '🎯🕗😕🐬'
 baseEmoji.toNames(buf)
 // => ':dart::clock8::confused::dolphin:'
-e.toCustom(buf, function(v, emojiName, emojiData) {
-  return '<img src="/img/emoji/'+emojiName+'.png" alt="'+emojiData.character+'" title="'+emojiName+'">'
+e.toCustom(buf, function(v, emoji) {
+  return '<img src="/img/emoji/'+emoji.name+'.png" alt="'+emoji.char+'" title="'+emoji.name+'">'
 })
 // => '<img src="/img/emoji/dart.png" alt="🎯" title="dart">...'
 ```
+
+## encoding
+
+The emojis used are in `emojis.json`. There are 854 emojis there, so the converter reads sequences of 9 bits at a time and maps the value to the first 512 of them. To stay consistent with other renderings, make sure you don't change the order of your emojis.json.
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var emojis = require('emoji-named-characters')
-var emojiSubset = Object.keys(emojis).slice(0, 256)
+var bitparser = require('bitparser')
+var emojis = require('./emojis.json')
 
 function toBuffer(v) {
   if (!Buffer.isBuffer(v))
@@ -8,28 +8,48 @@ function toBuffer(v) {
 }
 
 function convert(buf, fn) {
-  buf = toBuffer(buf)
   var out = ''
-  for (var i = 0; i < buf.length; i++) {
-    out += fn(buf.readUInt8(i))
+  buf = toBuffer(buf)
+  var bp = bitparser(buf)
+  while (bp.index < buf.length * 8) {
+    try {
+      out += fn(bp.readBits(9))
+    } catch (e) {
+      break
+    }
   }
   return out
 }
 
 exports.toUtf8 = function(buf) {
   return convert(buf, function(c) {
-    return emojis[emojiSubset[c]].character
+    return emojis[c].char
   })
 }
 
 exports.toNames = function(buf) {
   return convert(buf, function(c) {
-    return ':'+emojiSubset[c]+':'
+    return ':'+emojis[c].name+':'
   })
 }
 
 exports.toCustom = function(buf, fn) {
   return convert(buf, function(c) {
-    return fn(c, emojiSubset[c], emojis[emojiSubset[c]])
+    return fn(c, emojis[c])
   })
+}
+
+if (!module.parent) {
+  console.log('toUtf8: deadbeef')
+  console.log(exports.toUtf8('deadbeef'))
+  console.log('')
+  console.log('toUtf8: 6ecd355b90e81e72c0e37c3b4023325b20f048c58d4200c69e4fbd550876e6fe')
+  console.log(exports.toUtf8('6ecd355b90e81e72c0e37c3b4023325b20f048c58d4200c69e4fbd550876e6fe'))
+  console.log('')
+  console.log('toNames: deadbeef')
+  console.log(exports.toNames('deadbeef'))
+  console.log('')
+  console.log('toNames: 6ecd355b90e81e72c0e37c3b4023325b20f048c58d4200c69e4fbd550876e6fe')
+  console.log(exports.toNames('6ecd355b90e81e72c0e37c3b4023325b20f048c58d4200c69e4fbd550876e6fe'))
+  console.log('')
 }
